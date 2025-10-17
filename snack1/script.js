8ca4ff376c7b3f02b52a185f196780a12867f44e
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-10-17 15:34:56 +0200

    snack1 pseudocode

--- snack1/script.js
+++ snack1/script.js
@@ -4,3 +4,8 @@ Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore.
 */
+
+//creo l'array di oggetti
+//all'interno delgi oggetti inserisco il nome ed il peso della bicicletta (proprietà)
+//creo un ciclo for che scorre l'arrray di oggetti
+//imposto delle consizioni per selezionare solo la bici con il peso minore
