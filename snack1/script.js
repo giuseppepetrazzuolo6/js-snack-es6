7ab50bb714bb42dc34c97ac90216301f10082fb2
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-10-17 15:41:50 +0200

    snack1 array of object

--- snack1/script.js
+++ snack1/script.js
@@ -6,6 +6,28 @@ Stampare a schermo la bici con peso minore.
 */
 
 //creo l'array di oggetti
+const biciclette = [
+    {
+        nome: 'Canyon Ultimate CF 7',
+        peso: 8.7
+    },
+    {
+        nome: 'Specialized S Works Tarmac SL8',
+        peso: 6.6
+    },
+    {
+        nome: 'Cube Litening AIR C:68X Pro',
+        peso: 7.1
+    },
+    {
+        nome: 'Trek Émonda SLR 9 Disc',
+        peso: 6.1
+    },
+    {
+        nome: 'De Rosa King',
+        peso: 6.5
+    }
+];
 //all'interno delgi oggetti inserisco il nome ed il peso della bicicletta (proprietà)
 //creo un ciclo for che scorre l'arrray di oggetti
 //imposto delle consizioni per selezionare solo la bici con il peso minore
